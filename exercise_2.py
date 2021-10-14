abef7666ff4722277c3197c284e9af09ad561632
ma2sevich222 <58733670+ma2sevich222@users.noreply.github.com>
2021-10-14 19:53:26 +1000

    Update exercise_2.py

--- exercise_2.py
+++ exercise_2.py
@@ -6,8 +6,8 @@ Created on Wed Oct 13 21:57:12 2021
 @author: ma2sevich
 """
 def odometer(N:[int])-> int:
-  speed = [ i for i in N if N.index(i) % 2 == 0]
-  time = [i for i in N if N.index(i) % 2 != 0]
+  speed = N[::2]
+  time = b[1::2]
   time.insert(0,0)
   dif_time=[time[i+1]-time[i] for i in range(len(time)-1)]
   return sum([x * y for x, y in zip(speed,dif_time)])
